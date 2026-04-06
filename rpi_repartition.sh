28a6b540215c0d14878208ae0003e4efa04072ee
Hank Barta <hbarta@gmail.com>
2026-04-06 12:36:30 -0400

    perms for /mnt/data

--- rpi_repartition.sh
+++ rpi_repartition.sh
@@ -258,11 +258,10 @@ resize2fs "${ROOT_PART}"
 info "Mounting root partition to update /etc/fstab..."
 mount "${ROOT_PART}" "${ROOT_MNT}"
 
-CRON_FILE="${ROOT_MNT}/etc/cron.d/mount_data"
+CRON_FILE="${ROOT_MNT}/etc/cron.d/mount_mnt_data"
 info "Creating cron entry to mount /mnt/data in ${CRON_FILE}"
 
-echo "@reboot root /bin/mount -t ext4  UUID=${DATA_UUID}  ${DATA_MOUNT_POINT}" > "${CRON_FILE}"
-echo "chmod 644 ${CRON_FILE}" >> "${CRON_FILE}"
+echo "@reboot root /bin/mount -t ext4  UUID=${DATA_UUID}  ${DATA_MOUNT_POINT} && /bin/chmod a+rwx ${DATA_MOUNT_POINT}"  > "${CRON_FILE}" 
 
 # Create the mount point directory on the root filesystem
 MOUNT_DIR="${ROOT_MNT}${DATA_MOUNT_POINT}"
